1c8bd1d6779127280a327bafbf8c120e6ce26751
Tharani Garikapati <tharanigarikapati@gmail.com>
2025-12-29 17:00:00 +0530

    Fix TypeScript errors: added clearLastDeleted to UseTasksState

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -23,6 +23,7 @@ interface UseTasksState {
   updateTask: (id: string, patch: Partial<Task>) => void;
   deleteTask: (id: string) => void;
   undoDelete: () => void;
+  clearLastDeleted: () => void;
 }
 
 const INITIAL_METRICS: Metrics = {
@@ -174,7 +175,7 @@ const clearLastDeleted = useCallback(() => {
   setLastDeleted(null);
 }, []);
 
-  return { tasks, loading, error, derivedSorted, metrics, lastDeleted, addTask, updateTask, deleteTask, undoDelete,clearLastDeleted };
+  return { tasks, loading, error, derivedSorted, metrics, lastDeleted, addTask, updateTask, deleteTask, undoDelete, clearLastDeleted};
 }
 
 
